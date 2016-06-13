66fe8da11b6b6fabba5c28b04a10b34d4a7055f6
Kseniya <kseniya.panasyuk@gmail.com>
2016-06-12 21:46:23 -0400

    Added table size test

--- src/test/java/sample/dbunit/jpa/CustomerRepositoryWithDataSupportTest.java
+++ src/test/java/sample/dbunit/jpa/CustomerRepositoryWithDataSupportTest.java
@@ -2,6 +2,7 @@ package sample.dbunit.jpa;
 
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.google.common.collect.Iterables;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +15,11 @@ import org.springframework.test.context.support.DirtiesContextTestExecutionListe
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import sample.dbunit.jpa.utils.DbUnitConnectionSetter;
 import sample.dbunit.jpa.entities.Customer;
-import sample.dbunit.jpa.utils.tables.SampleTables;
 
-import static org.dbunit.data.support.DbUnitDataUtils.cleanInsert;
+import static org.dbunit.data.support.DbUnitAssertionUtils.assertIsEmpty;
+import static org.dbunit.data.support.DbUnitAssertionUtils.assertSize;
+import static org.dbunit.data.support.DbUnitDataUtils.clean;
+import static org.dbunit.data.support.DbUnitDataUtils.insert;
 import static org.dbunit.data.support.DbUnitDataUtils.table;
 import static org.dbunit.data.support.DbUnitDataUtils.with;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -25,6 +28,7 @@ import static org.hamcrest.core.IsNull.notNullValue;
 import static sample.dbunit.jpa.utils.tables.Customers.FIRST_NAME;
 import static sample.dbunit.jpa.utils.tables.Customers.ID;
 import static sample.dbunit.jpa.utils.tables.Customers.LAST_NAME;
+import static sample.dbunit.jpa.utils.tables.SampleTables.CUSTOMERS;
 
 /**
  * @author Kseniya Panasyuk
@@ -33,21 +37,33 @@ import static sample.dbunit.jpa.utils.tables.Customers.LAST_NAME;
 @SpringApplicationConfiguration(classes = Application.class)
 @ActiveProfiles("local")
 @TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
-		DirtiesContextTestExecutionListener.class,
-		TransactionalTestExecutionListener.class,
-		DbUnitTestExecutionListener.class,
-		DbUnitConnectionSetter.class})
+        DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class,
+        DbUnitConnectionSetter.class})
 public class CustomerRepositoryWithDataSupportTest {
 
-	@Autowired
-	private CustomerRepository repository;
+    @Autowired
+    private CustomerRepository repository;
 
-	@Test
-	public void testContextLoads() throws Exception {
-		cleanInsert(table(SampleTables.CUSTOMERS).withRow(with(ID, 1), with(FIRST_NAME, "Fiery"), with(LAST_NAME, "Phoenix")));
-		Iterable<Customer> all = repository.findAll();
-		assertThat(all, is(notNullValue()));
-		assertThat(Iterables.size(all), is(1));
-	}
+    @Before
+    public void before() {
+        clean(CUSTOMERS);
+    }
+
+    @Test
+    public void testContextLoads() throws Exception {
+        insert(table(CUSTOMERS).withRow(with(ID, 1), with(FIRST_NAME, "Fiery"), with(LAST_NAME, "Phoenix")));
+        Iterable<Customer> all = repository.findAll();
+        assertThat(all, is(notNullValue()));
+        assertThat(Iterables.size(all), is(1));
+    }
+
+    @Test
+    public void testSave() throws Exception {
+        assertIsEmpty(CUSTOMERS);
+        repository.save(new Customer("John", "Dou"));
+        assertSize(CUSTOMERS, 1);
+    }
 
 }
